522cc3f7fe7f6b90c692a1fe414a9752cc91ffb4
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-11-15 00:03:16 -0300

    first commit

---
+++ src/main/java/br/com/fillipeoliveira/coursehub_api/CoursehubApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fillipeoliveira.coursehub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursehubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursehubApiApplication.class, args);
+	}
+
+}
