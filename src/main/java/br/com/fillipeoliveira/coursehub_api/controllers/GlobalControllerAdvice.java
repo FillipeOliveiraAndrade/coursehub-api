80839727d5223fe71bbe4e5607f564797d1a4757
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-11-16 15:57:50 -0300

    Alterando o nome da classe CourseNotFound

--- src/main/java/br/com/fillipeoliveira/coursehub_api/controllers/GlobalControllerAdvice.java
+++ src/main/java/br/com/fillipeoliveira/coursehub_api/controllers/GlobalControllerAdvice.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import br.com.fillipeoliveira.coursehub_api.exceptions.CourseConflictException;
-import br.com.fillipeoliveira.coursehub_api.exceptions.CourseNotFound;
+import br.com.fillipeoliveira.coursehub_api.exceptions.CourseNotFoundException;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
   @ExceptionHandler({
     CourseConflictException.class,
-    CourseNotFound.class
+    CourseNotFoundException.class
   })
   public ResponseEntity<String> handleExceptionResponse(RuntimeException exception) {
     return ResponseEntity.badRequest().body(exception.getMessage());
