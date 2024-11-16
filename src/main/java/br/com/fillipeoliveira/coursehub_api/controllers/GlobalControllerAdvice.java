df686b61a472f038bd771ee5bde79c894d0a7016
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-11-16 15:56:44 -0300

    Adicionando as class CourseConflictException e CourseNotFoundException ao GlobalControllerAdvice

--- src/main/java/br/com/fillipeoliveira/coursehub_api/controllers/GlobalControllerAdvice.java
+++ src/main/java/br/com/fillipeoliveira/coursehub_api/controllers/GlobalControllerAdvice.java
@@ -2,9 +2,18 @@ package br.com.fillipeoliveira.coursehub_api.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.fillipeoliveira.coursehub_api.exceptions.CourseConflictException;
+import br.com.fillipeoliveira.coursehub_api.exceptions.CourseNotFound;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
+
+  @ExceptionHandler({
+    CourseConflictException.class,
+    CourseNotFound.class
+  })
   public ResponseEntity<String> handleExceptionResponse(RuntimeException exception) {
     return ResponseEntity.badRequest().body(exception.getMessage());
   }
