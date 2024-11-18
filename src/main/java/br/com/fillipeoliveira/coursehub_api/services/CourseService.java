f45738f93ae42565f0b6db6b5904e9be9b770c0e
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-11-18 00:03:55 -0300

    Alterando a função de get no arquivo service

--- src/main/java/br/com/fillipeoliveira/coursehub_api/services/CourseService.java
+++ src/main/java/br/com/fillipeoliveira/coursehub_api/services/CourseService.java
@@ -1,5 +1,6 @@
 package br.com.fillipeoliveira.coursehub_api.services;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,8 +27,12 @@ public class CourseService {
     return this.courseRepository.save(newCourse);
   }
 
-  public List<Course> findAll() {
-    List<Course> courses = this.courseRepository.findAll();
-    return courses;
+  public List<Course> findCoursesWithFiltersOrNot(String name, String category) {
+    if (name == null && category == null) return this.courseRepository.findAll();
+    if (name == null) return this.courseRepository.findByNameOrCategory(null, category); // Filtrando apenas pela category
+    if (category == null) return this.courseRepository.findByNameOrCategory(name, null); // Filtrando apenas pelo nome
+
+    Optional<Course> courses = this.courseRepository.findByNameAndCategory(name, category);
+    return courses.map(Collections::singletonList).orElse(Collections.emptyList()); // convertendo o optional para list (reaproveitando a função findByNameAndCategory)
   }
 }
