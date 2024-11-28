4ae2b5e70a64101474010c825b2da3c033f4d3da
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-11-28 15:53:24 -0300

    Alterações na GlobalControllerAdvice

--- src/main/java/br/com/fillipeoliveira/coursehub_api/controllers/GlobalControllerAdvice.java
+++ src/main/java/br/com/fillipeoliveira/coursehub_api/controllers/GlobalControllerAdvice.java
@@ -1,20 +1,38 @@
 package br.com.fillipeoliveira.coursehub_api.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import br.com.fillipeoliveira.coursehub_api.exceptions.CourseConflictException;
 import br.com.fillipeoliveira.coursehub_api.exceptions.CourseNotFoundException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-  @ExceptionHandler({
-    CourseConflictException.class,
-    CourseNotFoundException.class
-  })
-  public ResponseEntity<String> handleExceptionResponse(RuntimeException exception) {
-    return ResponseEntity.badRequest().body(exception.getMessage());
-  }
+    @ExceptionHandler({
+      CourseConflictException.class,
+      CourseNotFoundException.class
+    })
+    public ResponseEntity<String> handleExceptionResponse(RuntimeException exception) {
+      return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+      Map<String, String> errors = new HashMap<>();
+      ex.getBindingResult().getAllErrors().forEach(error -> {
+          String fieldName = ((FieldError) error).getField();
+          String errorMessage = error.getDefaultMessage();
+          errors.put(fieldName, errorMessage);
+      });
+      
+      return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
 }
