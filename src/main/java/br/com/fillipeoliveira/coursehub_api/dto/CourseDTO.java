73f705527a134eec2d7d6757aab3e3005d7bbe02
Fillipe Oliveira <fillipe.andrade20@gmail.com>
2024-11-28 15:57:02 -0300

    Refatorando a entidade CourseDTO

--- src/main/java/br/com/fillipeoliveira/coursehub_api/dto/CourseDTO.java
+++ src/main/java/br/com/fillipeoliveira/coursehub_api/dto/CourseDTO.java
@@ -3,11 +3,16 @@ package br.com.fillipeoliveira.coursehub_api.dto;
 import br.com.fillipeoliveira.coursehub_api.models.entities.Course;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record CourseDTO(
+  @NotBlank(message = "Course name must not be blank.")
   @Schema(example = "Curso de Java", requiredMode = RequiredMode.REQUIRED)
   String name,
-  
+
+  @NotBlank(message = "Course category must not be blank.")
   @Schema(example = "Back-End", requiredMode = RequiredMode.REQUIRED)
   String category,
 
